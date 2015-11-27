086a477c1576acf1e5c18e9695b32aeebddab2c9
Jonathan Svenheden <jonathan@svenheden.com>
2015-11-27 16:40:49 -0500

    Add support for using external URL's

--- index.js
+++ index.js
@@ -3,21 +3,24 @@
 const cleanHtml = require('./lib/clean-html');
 const compareDoms = require('./lib/compare-doms');
 const createDom = require('./lib/create-dom');
+const isUrl = require('is-url');
 const printToConsole = require('./lib/print-to-console');
 const readFile = require('./lib/read-file');
+const readUrl = require('request-promise');
 
+const readResource = resource => (isUrl(resource) ? readUrl : readFile)(resource);
 const getDifferences = result => result.getDifferences();
 
 if (process.argv.length < 4) {
-  console.error('You need to provide two files to compare');
+  console.error('You need to provide a pair of URL\'s or files to compare');
 } else {
-  const files = process.argv.slice(2, 4);
+  const resources = process.argv.slice(2, 4);
 
-  Promise.all(files.map(readFile))
+  Promise.all(resources.map(readResource))
     .then(content => Promise.all(content.map(cleanHtml)))
     .then(html => Promise.all(html.map(createDom)))
     .then(compareDoms)
     .then(getDifferences)
     .then(printToConsole)
-    .catch(console.error);
+    .catch(err => console.error(err.message));
 }
